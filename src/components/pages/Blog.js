7297bda7a4f5d275600ae99df5b12a1fa4a396e7
TonyNganga <antonynganga247@gmail.com>
2022-03-07 11:51:16 +0300

    added a page to add a new blog

--- src/components/pages/Blog.js
+++ src/components/pages/Blog.js
@@ -42,42 +42,43 @@ function Blog() {
     //         return '';
     //     };
 
-    //     const getBlogs = () => {
-    //         let list = [];
-    //         let result = [];
+    //code to retrieve all the blogs
+        const getBlogs = () => {
+            let list = [];
+            let result = [];
 
-    //         blogs.map(blogPost => {
-    //             return list.push(
-    //                 <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-    //                     <div className="col p-4 d-flex flex-column position-static">
-    //                         <strong className="d-inline-block mb-2 text-primary">{capitalizeFirstLetter(blogPost.category)}</strong>
-    //                         <h3 className="mb-0">{blogPost.title}</h3>
-    //                         <div className="mb-1 text-muted">{blogPost.month} {blogPost.day}</div>
-    //                         <p className="card-text mb-auto">{blogPost.excerpt}</p>
-    //                         <Link to={`/blog/${blogPost.slug}`} className="stretched-link">Continue reading</Link>
-    //                     </div>
-    //                     <div className="col-auto d-none d-lg-block">
-    //                         <img width='200' height='250' src={blogPost.thumbnail} alt='thumbnail' />
-    //                     </div>
-    //                 </div>
-    //             );
-    //         });
+            blogs.map(blogPost => {
+                return list.push(
+                    <div className="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                        <div className="col p-4 d-flex flex-column position-static">
+                            <strong className="d-inline-block mb-2 text-primary">{capitalizeFirstLetter(blogPost.category)}</strong>
+                            <h3 className="mb-0">{blogPost.title}</h3>
+                            <div className="mb-1 text-muted">{blogPost.month} {blogPost.day}</div>
+                            <p className="card-text mb-auto">{blogPost.excerpt}</p>
+                            <Link to={`/blog/${blogPost.slug}`} className="stretched-link">Continue reading</Link>
+                        </div>
+                        <div className="col-auto d-none d-lg-block">
+                            <img width='200' height='250' src={blogPost.thumbnail} alt='thumbnail' />
+                        </div>
+                    </div>
+                );
+            });
 
-    //         for (let i = 0; i < list.length; i += 2) {
-    //             result.push(
-    //                 <div key={i} className='row mb-2'>
-    //                     <div className='col-md-6'>
-    //                         {list[i]}
-    //                     </div>
-    //                     <div className='col-md-6'>
-    //                         {list[i+1] ? list[i+1] : null}
-    //                     </div>
-    //                 </div>
-    //             )
-    //         }
+            for (let i = 0; i < list.length; i += 2) {
+                result.push(
+                    <div key={i} className='row mb-2'>
+                        <div className='col-md-6'>
+                            {list[i]}
+                        </div>
+                        <div className='col-md-6'>
+                            {list[i+1] ? list[i+1] : null}
+                        </div>
+                    </div>
+                )
+            }
 
-    //         return result;
-    //     };
+            return result;
+        };
 
     return (
         <div>
--- src/components/pages/NewPost.js
+++ src/components/pages/NewPost.js
@@ -1,7 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import propTypes from 'prop-types';
 // import {addPost, updatePost} from '../../action/posts';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
+import BlogNavbar from '../BlogNavbar';
 
 class NewPost extends Component {
   // constructor (props) {
@@ -35,43 +36,60 @@ class NewPost extends Component {
   //     });
   //   }
   // };
-  render () {
+  render() {
     // if (this.props.update == true && this.props.location.state == undefined)
     //   return <h2>404 Page not Found</h2>;
     // const {title, content} = this.state;
     return (
-      <div className="d-flex justify-content-center mt-5">
-        <form >
-          <fieldset className="form-group">
-            <legend className="border-bottom  ml-4">Blog Post</legend>
-            <div className="form-group  mt-4">
-              <label>Title</label>
-              <input
-                type="text"
-                className="form-control mt-2"
-                name="title"
+      <div>
+        <div className='blognav ml-5'>
+          <BlogNavbar />
+        </div>
+        <div className="d-flex justify-content-center mt-5">
+
+          <form >
+            <fieldset className="form-group">
+              <legend className="border-bottom  ml-4">Blog Post</legend>
+              {/* <div className="uploadimage__form">
+                <img id="img" alt="#" width="100px" height="100px" />
+                <input type="file" accept="image/*" name="image-upload" id="input" />
+                <div className="upload__btn">
+                  <label className="image-upload" htmlFor="input">
+                    <i className="material-icons">add_photo_alternate</i>
+                    {'Choose file'}
+                  </label>
+                </div>
+              </div> */}
+              <div className="form-group  mt-2">
+                <label>Title</label>
+                <input
+                  type="text"
+                  className="form-control mt-2"
+                  name="title"
                 // value={title}
                 // onChange={this.onChangeHandler}
-              />
-            </div>
-            <div className="form-group">
-              <label>Content</label>
-              <textarea
-                type="text"
-                className="form-control"
-                name="content"
+                />
+              </div>
+              <div className="form-group">
+                <label>Content</label>
+                <textarea
+                  type="text"
+                  className="form-control"
+                  name="content"
                 // value={content}
                 // onChange={this.onChangeHandler}
-              />
-            </div>
-            <div className="form-group mt-3">
-              <button type="submit" className="btn btn btn-outline-info">
-                Post
-              </button>
-            </div>
-          </fieldset>
-        </form>
+                />
+              </div>
+              <div className="form-group mt-3">
+                <button type="submit" className="btn btn btn-outline-info">
+                  Post
+                </button>
+              </div>
+            </fieldset>
+          </form>
+        </div>
       </div>
+
     );
   }
 }
