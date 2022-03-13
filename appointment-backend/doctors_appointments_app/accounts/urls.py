1f2036d9532887019fa8331051e89a01a453a4a9
vicmwass <vicmwas2017@gmail.com>
2022-03-13 22:30:38 +0300

    clean urls files

--- appointment-backend/doctors_appointments_app/accounts/urls.py
+++ appointment-backend/doctors_appointments_app/accounts/urls.py
@@ -1,11 +1,8 @@
 from django.urls import path,include
 from .views import LoginAPI,RegisterAPI,ProfileAPI
 from knox import views as knox_views
-from rest_framework import routers
 
 
-route=routers.DefaultRouter()
-# route.register('auth/profile',ProfileAPI.as_view(),'profile')
 
 urlpatterns = [
     path('auth',include('knox.urls')),
@@ -14,5 +11,3 @@ urlpatterns = [
     path('auth/login',LoginAPI.as_view(),name='login'),
     path('auth/logout',knox_views.LogoutView.as_view(),name='knox_logout'),
 ]
-
-urlpatterns += route.urls
