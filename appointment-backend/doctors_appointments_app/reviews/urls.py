fb18cbdd51af5695d94f5de9dfed351a8b8ab9a8
vicmwass <vicmwas2017@gmail.com>
2022-03-26 14:49:15 +0300

    added retrive review to backend

--- appointment-backend/doctors_appointments_app/reviews/urls.py
+++ appointment-backend/doctors_appointments_app/reviews/urls.py
@@ -4,4 +4,5 @@ from .views import Reviews_API
 urlpatterns = [    
     path('reviews/post_review',Reviews_API.as_view({'post':'create'})),
     path('reviews/get_reviews',Reviews_API.as_view({'get':'list'})),
+    path('reviews/get_review',Reviews_API.as_view({'get':'retrieve'})),
 ]
