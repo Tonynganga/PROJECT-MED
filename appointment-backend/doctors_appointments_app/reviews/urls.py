64477983effab90083fddf5757c9977c30ec47d8
vicmwass <vicmwas2017@gmail.com>
2022-04-12 09:19:04 +0300

    gitignore changes

--- appointment-backend/doctors_appointments_app/reviews/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import Reviews_API
-
-urlpatterns = [    
-    path('reviews/post_review',Reviews_API.as_view({'post':'create'}),name='post_reviews'),
-    path('reviews/get_reviews',Reviews_API.as_view({'get':'list'}),name='get_reviews'),
-    path('reviews/get_review',Reviews_API.as_view({'get':'retrieve'}),name='get_review'),
-    path('reviews/update_review',Reviews_API.as_view({'put':'update'}),name='update_review'),
-]
--- src/utils.js
+++
@@ -1,5 +0,0 @@
-export const capitalizeFirstLetter = (word) => {
-    if (word)
-        return word.charAt(0).toUpperCase() + word.slice(1);
-    return '';
-  };
