33845e4c987ea64c969e9339d92751d5d6c822cd
vicmwass <vicmwass@vicmwas>
2023-09-20 11:35:19 +0300

    deploy to heroku test2

--- appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
+++ appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
@@ -153,6 +153,7 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR.parent.parent,'build/static')
 
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR.parent.parent,'build/static')
