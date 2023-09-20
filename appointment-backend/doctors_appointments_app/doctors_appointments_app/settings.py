5f43c09e696b092268e3b731c95703572fd45d38
vicmwass <vicmwass@vicmwas>
2023-09-20 12:27:41 +0300

    deploy to heroku test6

--- appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
+++ appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
@@ -102,7 +102,7 @@ CHANNEL_LAYERS = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3'
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': 'db.sqlite3',
     }
 #     'default':{
 #       'ENGINE':'django.db.backends.postgresql_psycopg2',
