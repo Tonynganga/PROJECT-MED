a3dda1afbcff6e2697d763c6ce4beed51c9765fc
vicmwass <vicmwass@vicmwas>
2023-09-20 11:38:22 +0300

    deploy to heroku test3

--- appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
+++ appointment-backend/doctors_appointments_app/doctors_appointments_app/settings.py
@@ -155,9 +155,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR.parent.parent,'build/static')
 
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR.parent.parent,'build/static')
-]
+# STATICFILES_DIRS=[
+#     os.path.join(BASE_DIR.parent.parent,'build/static')
+# ]
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 MEDIA_URL='/media/'
 REST_FRAMEWORK = {
