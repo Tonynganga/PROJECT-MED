9b8ad92b365966a9c37f8a4d9de6b79c8cdefaee
vicmwass <vicmwass@vicmwas>
2023-09-28 17:03:15 +0300

    deploy to heroku test11

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -3,4 +3,4 @@ import multiprocessing
 bind = "0.0.0.0:8000"  # Set the host and port
 workers = 3  # Adjust the number of worker processes
 timeout = 120
-chdir = '/home/vicmwass/PROJECT-MED/appointment-backend/doctors_appointments_app'
+chdir = './appointment-backend/doctors_appointments_app'
